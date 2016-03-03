40a26d520822adabefc578d869ee0dc195ccca1d
kml_c_16 <Bilovus@kml.kh.ua>
2016-03-03 09:30:49 +0200

    Out on screen "Code char = x"

---
+++ src/test1.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+/**
+ * Created by kml_c_16 on 2/25/2016.
+ */
+public class test1 {
+    public static void main(String[] args) throws IOException {
+        int x;
+        x = System.in.read();
+        char c = (char)x;
+        System.out.println("Code char" + c + "=" + x);
+
+    }
+}
